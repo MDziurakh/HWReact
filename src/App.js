fc28aa744a6801bca7f2800ecbe1d1b1bf9af63a
mdziurakh <mdzjurakh1@gmail.com>
2022-08-26 16:36:54 +0300

    hw3 v- with set and uuid in arr

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import './App.scss';
 import MyComponent from "./components/MyComponent/MyComponent";
 import Names from "./components/Names/Names";
-import { v4 as uuidv4 } from 'uuid';
 
 const namesArr =[
-    {name:"John", id:uuidv4()},
-    {name:'Joe', id:uuidv4()},
-    {name:'Baykar', id:uuidv4()},
-    {name:'Boris', id:uuidv4()},
-
+    'John', 'Joe', 'Baykar', 'Boris', 'John', 'Joe', 'Boris'
 ];
-
-
-
 
 function App() {
   return (
     <div className="App">
         <div className='container'>
-            <header>
-
-            </header>
+            <header></header>
             <main>
                 <MyComponent />
             </main>
--- src/components/Names/Names.js
+++ src/components/Names/Names.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import './Names.scss'
+import './Names.scss';
+import {v4 as uuidv4} from "uuid";
 
 
 const Names = (props) => {
 
-    const arr = props.names.map((item) =>
-    <li key={item.id}> {item.name}</li>
-    )
+
+    const uniqueNames = new Set(props.names);
+    const uniqueArr = Array.from(uniqueNames);
+    const lastArr = uniqueArr.map((name) =>({name: name, id:uuidv4()}))
+
+    const arr = lastArr.map((name)=>(
+        <li key={name.id}>{name.name}</li>
+    ))
 
     return (
         <div>
